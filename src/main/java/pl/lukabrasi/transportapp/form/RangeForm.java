f951ca3a07aab301a243d32d6430d80b3c54cd68
Luca Brasi <lukasz.anaczkowski@gmail.com>
2019-03-28 18:06:31 +0100

    - wybor zleceń z zakresu dat OK, eksport do csv i txt (prawie ok - entery) - excel export TBD - status zlecenia #status# - poprawka paginacji, fragment - dodawanie i edycja przewoźników - update rozladunkow (do zrobienia LinkedHashSet) - dodawanie rozładunków - dodawanie i edycja fabryk - widok tabeli i dodawanie tury bez obiektów - formularz i dodawanie tury TBD - poprawny JSON dla /orders findAll() - bootstrap - baza danych i wyświetlanie wszystkich zleceń

---
+++ src/main/java/pl/lukabrasi/transportapp/form/RangeForm.java
@@ -0,0 +1,13 @@
+package pl.lukabrasi.transportapp.form;
+
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+import java.time.LocalDate;
+
+@Data
+public class RangeForm {
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate date1;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate date2;
+}
