5ad4f4138742994eef29829ed70b845bad619032
Luca Brasi <lukasz.anaczkowski@gmail.com>
2020-01-15 17:34:38 +0100

    RELEASE 3.1.0 - poprawki wyglądu nasze auta RELEASE 3.1.0 - trasy google dla zlecen - poprawki wizualne RELEASE 3.0.1 - poprawki m.in query week mtw - kierowcy is exist i inne RELEASE 3.0.0. - nasz auta - kierowcy - dodawnia przewoznikow w locie i inne... - c.d. wprowadzania zakładki nasze auta - jQuery wybór lub dodanie przewoźnika do tury RELEASE 2.1.0 - kolumna KILOMETRY w zleceniach - formatowanie cen bez przecinka RELEASE 2.0.1 - bugfix dodawania zakazów RELEASE 2.0.0 - lista ZAKAZY RELEASE 1.4.0 - zapisywanie i wyswietlanie adresu ip klienta dokonujacego zmiany zlecenia (działa!!:) RELEASE 1.3.0 - raport tygodniowy RELEASE 1.2.0 - wyświetlanie zalogowanego uzytkownika windows w edycji zlecenia - usuniecie 'wszystko' z paginacji RELEASE 1.1.2 - fixed column width % - poprawione wyswietlanie nr zlecenia substring RELEASE 1.1.1 - bugfix wyswietlania nr zlecenia over 1000 substring - poprawka exportu do pliku - dodanie kolumny GODZ. załadunku w zleceniach - wyszukiwanie tur po numerze, kilka poprawek i usprawnień - przyciski poprzedniego i nastepnego tygodnia - sortowanie listy przewozników w edycji, czas edycji tury, poprawki - RELEASE 1.0.0 - jquery total sum w raporcie miesiecznym i jquery export do csv - raport miesieczny / osoba - RELEASE 2.0.0 - poprawki błędów - wyświetlanie tylko nowej tury po dodaniu - formularz dodawanie miasta w kalkulatorze tras - numer zlecenia na podstawie id - sprzedaż tur z ostatniego roku wykres ok - pobieranie sprzedazy z aktualnego miesiaca - raporty - wykres miesięczny tbd - RELEASE 0.1.0 - kalkulator tras szablon + baza stawek - pokazywanie aktualnie wybranego zakresu dat w formularzu - nazwy linkow po polsku - trimowanie numeru tury - bugfix (usuwanie znaku przejscia karetki z edycji kodow) - zmiana koloru radiobutton, wielkosc komunikatow, edit order zostaje na tej samej stronie, komunikat poprawna edycja RELEASE 0.0.5 - komunikaty o poprawnym dodaniu lub błędzie przy dodawaniu tury - wyszukiwanie aktualny tydzień i drobne poprawki RELEASE 0.0.4 - wyszukiwanie wszystkich tur lub tylko do sprzedania wg zakresu RELEASE 0.0.3 - poprawki layoutu - bugfix RELEASE 0.0.2 - dodanie pola miasto załadunku - poprawki błędów c.d. - poprawki błędów - zmiana kodów rozładunków z relacji many-to-many na zwykly string, eksport txt csv OK - wybor zleceń z zakresu dat OK, eksport do csv i txt (prawie ok - entery) - excel export TBD - status zlecenia #status# - poprawka paginacji, fragment - dodawanie i edycja przewoźników - update rozladunkow (do zrobienia LinkedHashSet) - dodawanie rozładunków - dodawanie i edycja fabryk - widok tabeli i dodawanie tury bez obiektów - formularz i dodawanie tury TBD - poprawny JSON dla /orders findAll() - bootstrap - baza danych i wyświetlanie wszystkich zleceń

--- src/main/resources/static/js/jquery.dataTables.js
+++ src/main/resources/static/js/jquery.dataTables.js
@@ -4179,7 +4179,7 @@
 		var language = settings.oLanguage;
 		var previousSearch = settings.oPreviousSearch;
 		var features = settings.aanFeatures;
-		var input = '<input type="search" class="'+classes.sFilterInput+'"/>';
+		var input = '<input type="search" class="form-control mr-sm-2"/>';
 	
 		var str = language.sSearch;
 		str = str.match(/_INPUT_/) ?
